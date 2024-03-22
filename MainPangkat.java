c898d4fe0b0209b1c5277c77d615c8f44b16a62f
abhixyz1 <abhinayazrf@gmail.com>
2024-03-23 00:57:30 +0700

    update

--- MainPangkat.java
+++ MainPangkat.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class MainPangkat {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -6,28 +7,25 @@ public class MainPangkat {
         System.out.print("Masukkan jumlah elemen yang ingin dihitung : ");
         int elemen = sc.nextInt();
 
-        // deklarasi array
         Pangkat[] png = new Pangkat[elemen];
 
-        // input nilai dan pangkat
         for (int i = 0; i < elemen; i++) {
-            png[i] = new Pangkat(); // inisialisasi objek png[i] dari class Pangkat
-            System.out.print("Masukkan nilai yang akan dipangkatkan ke-"+(i+1)+" : ");
-            png[i].nilai = sc.nextInt();
-            System.out.print("Masukkan nilai pemangkat ke-"+(i+1)+" : ");
-            png[i].pangkat = sc.nextInt();
+            System.out.print("Masukkan nilai yang akan dipangkatkan ke-" + (i + 1) + " : ");
+            int nilai = sc.nextInt();
+            System.out.print("Masukkan nilai pemangkat ke-" + (i + 1) + " : ");
+            int pangkat = sc.nextInt();
+            png[i] = new Pangkat(nilai, pangkat); // Menggunakan konstruktor dengan parameter
         }
 
-        // output
         System.out.println("===================================================");
         System.out.println("Hasil Pangkat dengan Brute Force");
         for (int i = 0; i < elemen; i++) {
-            System.out.println("Nilai "+png[i].nilai+" pangkat "+png[i].pangkat+" adalah : "+png[i].pangkatBF(png[i].nilai, png[i].pangkat));
+            System.out.println("Nilai " + png[i].nilai + " pangkat " + png[i].pangkat + " adalah : " + png[i].pangkatBF());
         }
         System.out.println("===================================================");
         System.out.println("Hasil Pangkat dengan Divide and Conquer");
         for (int i = 0; i < elemen; i++) {
-            System.out.println("Nilai "+png[i].nilai+" pangkat "+png[i].pangkat+" adalah : "+png[i].pangkatDC(png[i].nilai, png[i].pangkat));
+            System.out.println("Nilai " + png[i].nilai + " pangkat " + png[i].pangkat + " adalah : " + png[i].pangkatDC());
         }
         System.out.println("===================================================");
     }
