f7add502330d094ec2e212225be976ae8b934be8
abhixyz1 <abhinayazrf@gmail.com>
2024-03-23 00:58:21 +0700

    update
    git commit -m update

--- Pangkat.java
+++ Pangkat.java
@@ -1,27 +1,34 @@
-// Nama : Muhammad Abhinaya Zurfa
-
 public class Pangkat {
     public int nilai, pangkat; // deklarasi variabel nilai dan pangkat
     
+    // Konstruktor dengan parameter untuk nilai dan pangkat
+    public Pangkat(int nilai, int pangkat) {
+        this.nilai = nilai;
+        this.pangkat = pangkat;
+    }
+
     // method pangkat dengan Brute Force
-    public int pangkatBF(int a, int n) {
-        int hasil = 1; 
-        for (int i = 0; i < n; i++) {
-            hasil = hasil * a;
+    public int pangkatBF() {
+        int hasil = 1;
+        for (int i = 0; i < this.pangkat; i++) {
+            hasil = hasil * this.nilai; 
         }
         return hasil;
     }
 
     // method pangkat dengan Divide and Conquer
-    public int pangkatDC(int a, int n) {
-        if (n == 0) { // jika n = 0
-            return 1; // hasilnya 1
-        }
-        else {
+    public int pangkatDC() {
+        return pangkatDC2(this.nilai, this.pangkat);
+    }
+
+    int pangkatDC2(int a, int n) {
+        if (n == 0) {
+            return 1;
+        } else {
             if (n % 2 == 1) // jika n ganjil
-            return (pangkatDC(a, n/2) * pangkatDC(a, n/2) *a); 
+                return (pangkatDC2(a, n/2) * pangkatDC2(a, n/2) * a);
             else // jika n genap
-            return (pangkatDC(a, n/2) * pangkatDC(a, n/2));
+                return (pangkatDC2(a, n/2) * pangkatDC2(a, n/2));
         }
     }
 }
